7e0354b7045c42be8374147a4e50139d9f4a8b20
Siddhartha Sadhukhan <siddhartha.sadhukhan.2014@gmail.com>
2019-02-28 00:54:19 +0530

    page no. 35

--- src/main/scala/com/sid/scala/common/GetSparkSession.scala
+++ src/main/scala/com/sid/scala/common/GetSparkSession.scala
@@ -4,13 +4,13 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 object GetSparkSession {
-  def getSession(): SparkSession ={
-    val conf = new SparkConf().setAppName("Sid").setMaster("yarn")
-    //val sc=new SparkContext(conf)
+  val conf = new SparkConf().setAppName("Sid").setMaster("yarn")
+  //val sc=new SparkContext(conf)
 
-    val spark = SparkSession.builder()
-      .config(conf)
-      .getOrCreate()
+  val spark = SparkSession.builder()
+    .config(conf)
+    .getOrCreate()
+  def getSession(): SparkSession ={
     return spark
   }
 }
--- src/main/scala/com/sid/scala/examples/csv/SchemaInferenceExample.scala
+++ src/main/scala/com/sid/scala/examples/csv/SchemaInferenceExample.scala
@@ -1,18 +1,52 @@
-  package com.sid.scala.examples.csv
+package com.sid.scala.examples.csv
 
-  import com.sid.scala.common.GetSparkSession
+import com.sid.scala.common.GetSparkSession
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions.desc
 
-  object SchemaInferenceExample {
-    def printMe( ) : Unit = {
-      val flightData2015 = GetSparkSession.getSession()
-        .read
-        .option("inferSchema", "true")
-        .option("header", "true")
-        .csv("/user/p2991042/data/flight-data/csv/2015-summary.csv")
-      println( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n***-----------------------***\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" )
-      println("()()()()()"+flightData2015.take(5))
-      println( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n***-----------------------***\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" )
-      flightData2015.show()
+object SchemaInferenceExample {
+  def printMe( ) : Unit = {
+    val spark=GetSparkSession.getSession()
+    val flightData2015 = GetSparkSession.getSession()
+      .read
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/user/p2991042/data/flight-data/csv/2015-summary.csv")
+    println( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n***-----------------------***\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" )
+    flightData2015.show()
+    println( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n***-----------------------***\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" )
+    val myArray: Array[Row] =flightData2015.take(5)
+    println( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n***-----------------------***\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" )
+    flightData2015.sort("count").explain()
+    println( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n***-----------------------***\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" )
+    flightData2015.createOrReplaceTempView("flight_data_2015")
 
-    }
+    val sqlWay = GetSparkSession.getSession().sql("""
+    SELECT DEST_COUNTRY_NAME, count(1)
+    FROM flight_data_2015
+    GROUP BY DEST_COUNTRY_NAME
+    """)
+    sqlWay.explain()
+    val dataFrameWay = flightData2015.groupBy("DEST_COUNTRY_NAME").count()
+
+    println( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n***-----------------------***\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" )
+    val maxSql = spark.sql("""
+    SELECT DEST_COUNTRY_NAME, sum(count) as destination_total
+    FROM flight_data_2015
+    GROUP BY DEST_COUNTRY_NAME
+    ORDER BY sum(count) DESC
+    LIMIT 5
+    """)
+
+    maxSql.explain()
+
+
+    val maxFrameWay=flightData2015.groupBy("DEST_COUNTRY_NAME").sum("count")
+      .withColumnRenamed("sum(count)", "destination_total")
+      .sort(desc("destination_total"))
+
+    maxSql.show()
+    maxFrameWay.explain()
+
   }
+}
